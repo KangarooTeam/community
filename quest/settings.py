63828f31a81fc6a131643b0afe35b2825d20b085
Lodhel <lodhel@yahoo.com>
2018-08-24 21:35:25 +0300

    настройки

--- quest/settings.py
+++ quest/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '%1z)@b2b&1=&xbpl^xwkj06@#i0ao#1pa26n9fe!1y2mj8lhv0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'books',
 ]
 
 MIDDLEWARE = [
@@ -103,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 
