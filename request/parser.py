8aaa1d7233173326463ac6f9f414991999efe6e4
Lodhel <lodhel@yahoo.com>
2018-08-28 19:52:41 +0300

    JSON

--- request/parser.py
+++ request/parser.py
@@ -1,6 +1,6 @@
 import requests
 import json
 
-a = requests.get('http://google.com')
-print(a.content)
-#result = json.loads(r.content.decode('utf-8'))
+a = requests.get('http://ipinfo.io/json')
+result = json.loads(a.content.decode('utf-8'))
+print(result)
